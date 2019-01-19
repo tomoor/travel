7942a98a705df7661bc655e4c2463e3288b5be4c
liuyang <1771142014>
2019-01-19 19:15:38 +0800

    init

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="icons">
+      <div class="icon">
+        <div class="icon-img">
+         <img src="@/assets/icon/mafengwo.png" class='icon-imgcontent' alt="">
+        </div>
+        <p class="icon-desc">找攻略</p>
+      </div>
+      <div class="icon">
+        <div class="icon-img">
+         <img src="@/assets/icon/mafengwo.png" class='icon-imgcontent' alt="">
+        </div>
+        <p class="icon-desc">热门景点</p>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeIcons"
+    }
+</script>
+
+<style scoped lang="stylus">
+  @import '~styles/varibles.styl'
+  .icons
+    overflow: hidden
+    height: 0
+    padding-bottom: 50%
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-imgcontent
+          display: block
+          margin: 0 auto
+          height:100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        heigt: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wraper">
+    <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.key">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeSwiper",
+    data () {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          loop:true
+        },
+        swiperList: [{
+          id: '0001',
+          imgUrl:'https://p2-q.mafengwo.net/s12/M00/0E/87/wKgED1w_TeK' +
+            'AcfyiAAghC45qhrI62.jpeg?imageMogr2%2Fthumbnail%2F%21750x4' +
+            '22r%2Fgravity%2FCenter%2Fcrop%2F%21750x422%2Fquality%2F90'
+        },{
+          id: '0002',
+          imgUrl:'https://b4-q.mafengwo.net/s12/M00/0D/AB/wKgED1w_TaaAE' +
+            'AVSAAacfZZM5l890.jpeg?imageMogr2%2Fthumbnail%2F%21750x422r' +
+            '%2Fgravity%2FCenter%2Fcrop%2F%21750x422%2Fquality%2F90'
+        },{
+          id: '0003',
+          imgUrl:'https://b2-q.mafengwo.net/s12/M00/B6/BD/wKgED1xALdiAUMq' +
+            'FAAXdW7D8e-w11.jpeg?imageMogr2%2Fthumbnail%2F%21750x422r%2Fg' +
+            'ravity%2FCenter%2Fcrop%2F%21750x422%2Fquality%2F90'
+        }]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+  .wraper >>> .swiper-pagination-bullet-active
+    background: #eeecf3
+  .wraper
+    overflow hidden
+    width: 100%
+    height: 0
+    padding-bottom: 56%
+    background: #dbdbdb
+    .swiper-img
+      width:100%
+
+</style>
